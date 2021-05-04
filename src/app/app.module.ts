754449cccc90b242d68b10e676e53416ae0597b7
Marc Wright <marc@generalassemb.ly>
2021-05-04 16:16:22 -0500

    clean up nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { CitiesComponent } from './cities/cities.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {CitiesComponent} from './cities/cities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CarouselComponent,
-    CitiesComponent
+    CitiesComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,4 +21,5 @@ import { CitiesComponent } from './cities/cities.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
