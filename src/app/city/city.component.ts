305095fb0af6a4271e7d687a7733a667676e1464
Marc Wright <marc@generalassemb.ly>
2021-05-04 16:47:39 -0500

    create city component

---
+++ src/app/city/city.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-city',
+  templateUrl: './city.component.html',
+  styleUrls: ['./city.component.css']
+})
+export class CityComponent implements OnInit {
+  @Input('cityIndex') cityIndex: number;
+  @Input('city') city: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
