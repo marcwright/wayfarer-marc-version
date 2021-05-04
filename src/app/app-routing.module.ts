adb144fe22ac8e17fd7a52fcefb5d17b7931e2f1
Marc Wright <marc@generalassemb.ly>
2021-05-04 13:25:02 -0500

    add bootstrap css and js files and add carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CarouselComponent} from './carousel/carousel.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+    path: 'home',
+    component: CarouselComponent
+  }
+  // {
+  //   path: 'resume',
+  //   component: ResumeComponent
+  // },
+  // {
+  //   path: 'links',
+  //   component: LinksComponent
+  // }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
